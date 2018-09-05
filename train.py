468ebce19c0a34b930d133fe5f45636723a0d9ae
maichmueller <m.aichmueller@gmail.com>
2018-09-05 21:33:15 +0200

    .

--- train.py
+++ train.py
@@ -54,7 +54,7 @@ def optimize_model(model):
 
     # Compute V(s_{t+1}) for all next states.
     next_state_values = torch.zeros(BATCH_SIZE).float()  # zero for terminal states
-    next_state_values[non_final_mask.cpu()] = model(non_final_next_states).max(1)[0].to(device) # what would the model predict
+    next_state_values[non_final_mask.cpu()] = model(non_final_next_states).max(1)[0].cpu() # what would the model predict
     with torch.no_grad():
         expected_state_action_values = (next_state_values * GAMMA) + reward_batch  # compute the expected Q values
 
